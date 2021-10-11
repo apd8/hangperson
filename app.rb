e5a4a15b1c05ba769ef1f7a4b6a34c1276bc9d4c
Ankit Darekar <apd8@njit.edu>
2021-10-10 22:23:46 -0400

    adding error handling

--- app.rb
+++ app.rb
@@ -44,9 +44,19 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if(!@game.guess(letter))
-      flash[:message]="Try a new letter. You have already used that letter."
-    end
+
+    begin 
+      if(!@game.guess(letter))
+        if(letter.nil? or /[^A-Za-z]/.match(letter) != nil or letter == '')
+          flash[:message]="Invalid Guess"
+        elsif
+          flash[:message]="You have already used that letter."
+        end
+      end  
+    rescue ArgumentError => e
+          warn e.message
+          flash[:message] = "Invalid guess."
+    end 
     redirect '/show'
   end
   
